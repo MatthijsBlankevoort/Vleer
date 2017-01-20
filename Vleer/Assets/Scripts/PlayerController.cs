17c78ce6d3047b4061601cfcbc24679799d1b2a9
Matthijs Blankevoort <Matthijsblankevoort@hotmail.com>
2017-01-20 23:03:55 +0100

    Added PlayerColor
    -PlayerColor script
    -Input for controller

--- Vleer/Assets/Scripts/PlayerController.cs
+++ Vleer/Assets/Scripts/PlayerController.cs
@@ -5,16 +5,37 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float movementSpeed;
+    private Color redColor, greenColor, blueColor, yellowColor;
+    private SpriteRenderer mySpriteRenderer;
 
 	// Use this for initialization
 	void Start () {
-		
+        redColor = new Color32(255,0,0,255);
+        greenColor = new Color32(0,255,0,255);
+        blueColor = new Color32(0,0,255,255);
+        yellowColor = new Color32(255,255,0,255);
+        mySpriteRenderer = this.GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         transform.Translate(Input.GetAxisRaw("Horizontal") * movementSpeed, Input.GetAxisRaw("Vertical") * movementSpeed, 0);
-        print(Input.GetAxisRaw("Horizontal"));
-	}
+        if (Input.GetKey(KeyCode.Joystick1Button0))
+        {
+            mySpriteRenderer.color = greenColor;
+        }
+        else if (Input.GetKeyDown(KeyCode.Joystick1Button1))
+        {
+            mySpriteRenderer.color = redColor;
+        }
+        else if (Input.GetKeyDown(KeyCode.Joystick1Button2))
+        {
+            mySpriteRenderer.color = blueColor;
+        }
+        else if (Input.GetKeyDown(KeyCode.Joystick1Button3))
+        {
+            mySpriteRenderer.color = yellowColor;
+        }
+    }
 }
     
