8a2788181b5ebeccda192130f71b64a63747f3a8
Matthijs Blankevoort <Matthijsblankevoort@hotmail.com>
2017-01-20 22:29:12 +0100

    Added Playermovement script
    -Changed Axis in InputMapper settings
    -Added PlayerMovement script
    -Tweaked MovementSpeed

---
+++ Vleer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float movementSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Input.GetAxisRaw("Horizontal") * movementSpeed, Input.GetAxisRaw("Vertical") * movementSpeed, 0);
+        print(Input.GetAxisRaw("Horizontal"));
+	}
+}
+    
