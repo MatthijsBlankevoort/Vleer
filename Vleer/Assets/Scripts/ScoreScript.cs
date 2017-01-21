8fb602f7f77a36c457312502502f59b283830c67
Matthijs Blankevoort <Matthijsblankevoort@hotmail.com>
2017-01-21 15:27:08 +0100

    Added scoresystem
    -Added scorescript
    -Added score
    -Added lives
    -Added collision based on color with enemies

---
+++ Vleer/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour {
+
+    public int score = 0;
+    public int lives = 3;
+    public GameObject[] enemies;
+
+    public SpriteRenderer mySpriteRenderer;
+    public SpriteRenderer enemySpriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+        mySpriteRenderer = this.GetComponent<SpriteRenderer>();
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            if (other.gameObject.GetComponent<SpriteRenderer>().color == mySpriteRenderer.color)
+            {
+                score++;
+                Destroy(other.gameObject);
+            }
+            else
+            {
+                lives--;
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
---
+++ Vleer/Assets/Scripts/SonarTestSpawner.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonarTestSpawner : MonoBehaviour
+{
+    public GameObject sonar;
+    public float growthSpeed, fadeSize, fadeTimer;
+    private Color color = Color.white;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            color = PlayerController.redColor;
+            print("red");
+        }
+        else if (Input.GetKeyDown(KeyCode.G))
+        {
+            color = PlayerController.greenColor;
+            print("green");
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            color = PlayerController.blueColor;
+            print("blue");
+        }
+        else if (Input.GetKeyDown(KeyCode.Y))
+        {
+            color = PlayerController.yellowColor;
+            print("yellow");
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            color = Color.white;
+            print("white");
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            spawnPosition.z = 0;
+            GameObject sonarRing = Instantiate(sonar, spawnPosition, Quaternion.identity) as GameObject;
+            SonarEffect effect = sonarRing.GetComponent<SonarEffect>();
+            effect.growthSpeed = growthSpeed;
+            effect.fadeSize = fadeSize;
+            effect.fadeTimer = fadeTimer;
+
+            SpriteRenderer sonarSpriteRenderer = sonarRing.GetComponent<SpriteRenderer>();
+            print(color);
+            sonarSpriteRenderer.color = new Color(color.r, color.g, color.b, sonarSpriteRenderer.color.a);
+            print(sonarSpriteRenderer.color);
+        }
+        
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            spawnPosition.z = 0;
+            GameObject sonarRing = Instantiate(sonar, spawnPosition, Quaternion.identity) as GameObject;
+            SonarEffect effect = sonarRing.GetComponent<SonarEffect>();
+            effect.growthSpeed = growthSpeed * 2;
+            effect.fadeSize = fadeSize * 2;
+            effect.fadeTimer = fadeTimer * 2;
+
+            SpriteRenderer sonarSpriteRenderer = sonarRing.GetComponent<SpriteRenderer>();
+            print(color);
+            sonarSpriteRenderer.color = new Color(color.r, color.g, color.b, sonarSpriteRenderer.color.a);
+            print(sonarSpriteRenderer.color);
+        }
+    }
+}
