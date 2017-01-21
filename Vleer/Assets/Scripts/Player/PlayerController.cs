b9111c49c37847e102fa27e1e04d8cb37e80dfb5
Orzadus <Orzadus@gmail.com>
2017-01-21 11:27:13 +0100

    Fixed merge errors

--- Vleer/Assets/Scripts/Player/PlayerController.cs
+++ Vleer/Assets/Scripts/Player/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour {
     public static Color32 redColor, greenColor, blueColor, yellowColor;
     private SpriteRenderer mySpriteRenderer;
     private PauseScript pause;
+    private Rigidbody2D rigidBody;
 
 	// Use this for initialization
 	void Start () {
@@ -17,13 +18,15 @@ public class PlayerController : MonoBehaviour {
         yellowColor = new Color32(255,235,0,255);
         mySpriteRenderer = this.GetComponent<SpriteRenderer>();
         pause = GameObject.FindGameObjectWithTag("GameController").GetComponent<PauseScript>();
+        rigidBody = this.gameObject.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (pause.isPaused)
             return;
-        transform.Translate(Input.GetAxisRaw("Horizontal") * movementSpeed * Time.deltaTime, Input.GetAxisRaw("Vertical") * movementSpeed * Time.deltaTime, 0);
+
+
         if (Input.GetKey(KeyCode.Joystick1Button0))
         {
             mySpriteRenderer.color = greenColor;
@@ -40,6 +43,43 @@ public class PlayerController : MonoBehaviour {
         {
             mySpriteRenderer.color = yellowColor;
         }
+
+
+        //Controller controls
+        rigidBody.velocity = Vector2.zero;
+        rigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * movementSpeed * Time.deltaTime, Input.GetAxisRaw("Vertical") * movementSpeed * Time.deltaTime);
+
+
+
+        /*
+        //Arrow keys controls for testing
+         rigidBody.velocity = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+
+            rigidBody.velocity += new Vector2(0,  movementSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+
+            rigidBody.velocity += new Vector2(0, -movementSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            rigidBody.velocity += new Vector2(-movementSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            rigidBody.velocity += new Vector2(movementSpeed * Time.deltaTime, 0);
+        }
+        if(!Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.DownArrow) && !Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidBody.velocity = Vector2.zero;
+        }
+        */
     }
 }
     
