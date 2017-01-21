de2afbf0366488278433f85ef6a557616ac31541
Ian van Heeren <ianvanheeren@hotmail.com>
2017-01-21 13:45:03 +0100

    Added Player Sonars
    Added:
      - Small player sonars every so many seconds
      - Big player sonars that can be charged
      - Big sonars can be fired with spacebar

---
+++ Vleer/Assets/Scripts/Player/PlayerSonar.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSonar : MonoBehaviour {
+
+    public float growthSpeed = 1.0f;
+    public float fadeSize = 1.0f;
+    public float fadeTimer = 1.0f;
+    public float secondsBetweenSonars = 2.0f;
+    public GameObject sonarPrefab;
+    public float bigGrowthSpeed = 2.0f;
+    public float bigFadeSize = 2.0f;
+    public float bigFadeTimer = 2.0f;
+    public GameObject bigSonarPrefab;
+    public float fullCharge = 100f;
+    public bool beginWithFullCharge;
+
+    private float timeFromNextSonar;
+    private float charge;
+
+	// Use this for initialization
+	void Start () {
+        timeFromNextSonar = secondsBetweenSonars;
+        if (beginWithFullCharge)
+            charge = fullCharge;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        timeFromNextSonar -= Time.deltaTime;
+
+        if (timeFromNextSonar <= 0)
+        {
+            GameObject spawnedSonar = (GameObject)Instantiate(sonarPrefab, gameObject.transform.position, gameObject.transform.rotation);
+
+            SonarEffect sonarScript = spawnedSonar.GetComponent<SonarEffect>();
+            sonarScript.growthSpeed = this.growthSpeed;
+            sonarScript.fadeSize = this.fadeSize;
+            sonarScript.fadeTimer = this.fadeTimer;
+
+            timeFromNextSonar = secondsBetweenSonars;
+        }
+
+        if (Input.GetButtonDown("Fire Large Sonar") && charge >= fullCharge)
+        {
+            charge = 0;
+            FireLargeSonar();
+        }
+	}
+
+    void FireLargeSonar()
+    {
+        GameObject spawnedSonar = (GameObject)Instantiate(bigSonarPrefab, gameObject.transform.position, gameObject.transform.rotation);
+
+        SonarEffect sonarScript = spawnedSonar.GetComponent<SonarEffect>();
+        sonarScript.growthSpeed = bigGrowthSpeed;
+        sonarScript.fadeSize = bigFadeSize;
+        sonarScript.fadeTimer = bigFadeTimer;
+    }
+
+    void Charge (float amount)
+    {
+        charge += amount;
+    }
+}
